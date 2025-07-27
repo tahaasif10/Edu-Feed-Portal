bcbb75c0262c52ae812a9ed3ecaad9a02a1f5ee5
yumna1623 <yumnanasir1623@gmail.com>
2025-07-27 00:30:05 -0700

    Final Hackathon

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AdminLoginPage from "./pages/AdminLoginPage";
 import AdminDashboard from "./pages/AdminDashboardPage";
-import FeedBackPage from "./Pages/FeedBackPage";
-import NavBarPage from "./Pages/NavBarPage";
-import ThankyouPage from "./Pages/ThankyouPage";
+// import FeedBackPage from "./Pages/FeedBackPage";
+// import NavBarPage from "./Pages/NavBarPage";
+// import ThankyouPage from "./Pages/ThankyouPage";
+import FeedBackPage from "./pages/FeedBackPage";
+import NavBarPage from "./pages/NavBarPage";
+import ThankyouPage from "./pages/ThankyouPage";
 
 function App() {
   return (
